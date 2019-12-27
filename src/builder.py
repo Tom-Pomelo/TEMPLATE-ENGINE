853aaa0486afc67a68ef95ee4ba22ee359b13ac3
Tom-Pomelo <jixingyou@sjtu.edu.cn>
2019-12-27 19:21:04 +0800

    add unit tests & pass all test cases

--- src/builder.py
+++ src/builder.py
@@ -1,10 +1,15 @@
 class CodeBuilder(object):
-    def __init__(self, indent=0):
+    def __init__(self, indent = 0):
         self.code = []
         self.indent_level = indent
 
     def add_line(self, line):
-        self.code.extend(' ' * self.indent_level, line, '\n')
+        self.code.extend([' ' * self.indent_level, line, '\n'])
+
+    def add_section(self):
+        section = CodeBuilder(self.indent_level)
+        self.code.append(section)
+        return section
 
     INDENT_STEP = 4
 
@@ -13,11 +18,6 @@ class CodeBuilder(object):
 
     def dedent(self):
         self.indent_level -= self.INDENT_STEP
-
-    def add_section(self):
-        section = CodeBuilder(self.indent_level)
-        self.code.append(section)
-        return section
 
     def __str__(self):
         return ''.join(str(c) for c in self.code)
